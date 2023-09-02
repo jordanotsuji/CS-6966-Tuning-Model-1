af9a499970c85dcb64fd8823d773fb290a0859c3
jordanotsuji <18375877+jordanotsuji@users.noreply.github.com>
2023-09-01 21:39:08 -0600

    changes

--- error_analysis.py
+++ error_analysis.py
@@ -1,8 +1,7 @@
 import jsonlines
 import datasets
-import torch
 import random
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer
 
 # from huggingface_hub import login
 # login()
@@ -11,35 +10,23 @@ dataset = datasets.load_dataset("imdb")
 
 # Load your locally saved model and tokenizer
 model_path = "/scratch/general/vast/u1253335/cs6966/assignment1/models/deberta-v3-base-finetuned-imdb/checkpoint-6250"
-tokenizer = AutoTokenizer.from_pretrained(model_path)
+tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True)
 model = AutoModelForSequenceClassification.from_pretrained(model_path)
+
+
+def preprocess_function(examples):
+    return tokenizer(examples["text"], truncation=True, padding="max_length", max_length=512)
+
+
+encoded_dataset = dataset.map(preprocess_function, batched=True)
 
 # list of your 10 instances in the format of a dictionary {'review': <review text>, 'label': <gold label>, 'predicted': <predicted label>}
 incorrect_predictions = []
 
-print("printing unsupervised ----------------------\n")
-print(dataset["unsupervised"][:10])
+trainer = Trainer(model)
+predictions = trainer.predict(encoded_dataset["unsupervised"])
 
-# Make predictions and evaluate
-for sample in dataset["unsupervised"]:
-    text = sample["text"]  # Assuming the "unsupervised" portion contains text data
-    # Tokenize and encode the input text
-    inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
-
-    # Make predictions
-    with torch.no_grad():
-        logits = model(**inputs).logits
-        predicted_label = logits.argmax().item()
-
-    # Since this is the "unsupervised" portion, there are no true labels.
-    # We consider predictions with high confidence (e.g., confidence < -1.0) as incorrect.
-    if logits.max().item() > -1.0:
-        incorrect_predictions.append({"review": text, "label": sample["label"], "predicted": predicted_label})
-
-# Randomly select 10 incorrect predictions
-random.seed(42)  # Set seed for reproducibility
-selected_incorrect_predictions = random.sample(incorrect_predictions, min(10, len(incorrect_predictions)))
-
+print(predictions.predictions[0])
 
 output_filename = "errors.txt"  # give a name
 # Save the selected incorrect predictions to a JSONL file
