79e35d4136a61f89fed5b20320ad3626ff05f543
jordanotsuji <18375877+jordanotsuji@users.noreply.github.com>
2023-09-01 22:55:54 -0600

    changes

--- error_analysis.py
+++ error_analysis.py
@@ -26,15 +26,53 @@ incorrect_predictions = []
 trainer = Trainer(model)
 predictions = trainer.predict(encoded_dataset["unsupervised"])
 
+# Make predictions
+predictions = trainer.predict(encoded_dataset["unsupervised"])
+
+# Get the predicted labels
+predicted_labels = [p.argmax() for p in predictions.predictions]
+
+# Get the true labels (None for unsupervised data)
+true_labels = encoded_dataset["unsupervised"]["label"]
+
+# Find indices of incorrect predictions
+incorrect_indices = [
+    i
+    for i, (true, predicted) in enumerate(zip(true_labels, predicted_labels))
+    if true is not None and true != predicted
+]
+
+# Randomly select 10 incorrect predictions
+random.seed(42)  # Set seed for reproducibility
+selected_incorrect_indices = random.sample(incorrect_indices, min(10, len(incorrect_indices)))
+
+# Prepare the data for writing to the file
+output_items = []
+
+for index in selected_incorrect_indices:
+    item = {
+        "review": encoded_dataset["unsupervised"]["text"][index],
+        "label": true_labels[index],
+        "predicted": predicted_labels[index],
+    }
+    output_items.append(item)
+
+output_filename = "errors.txt"  # Give a name for the output file
+
+# Save the selected incorrect predictions to a JSONL file
+with jsonlines.open(output_filename, mode="w") as writer:
+    for item in output_items:
+        writer.write(item)
+
 # print(predictions.predictions[0])
 # print(predictions.predictions[1])
 # print(predictions.label_ids)
 # print(predictions)
-print(predictions.predictions.size)
-print(predictions.label_ids.size)
+# print(predictions.predictions.size)
+# print(predictions.label_ids.size)
 
-print("\n\n--------------------------------------------------------------\n")
-print(dataset["unsupervised"][0])
+# print("\n\n--------------------------------------------------------------\n")
+# print(dataset["unsupervised"][0])
 
 # output_filename = "errors.txt"  # give a name
 # # Save the selected incorrect predictions to a JSONL file
